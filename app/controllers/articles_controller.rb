a521828c36afa27ed60dc3d85a4b28c92fff6890
William Seyler <william.seyler@hitachivantara.com>
2019-11-11 15:56:40 -0500

    Added the editing of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,20 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was sucessfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
