26b3cc7eb6361da3067a1a84aa6754f9ddaad7f9
William E. Seyler <wseyler@pentaho.com>
2019-11-17 21:03:51 -0500

    add association between user and articles and add debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was sucessfully created"
       redirect_to article_path(@article)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :articles
+  before_save { self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: { case_sensative: false },
             length: { minimum: 3, maximum: 25 }
