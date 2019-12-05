b3c87f30c9ec210f6f7c4f7b90d9927695a3226c
William Seyler <william.seyler@hitachivantara.com>
2019-12-05 11:17:59 -0500

    Made field accessor private.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
